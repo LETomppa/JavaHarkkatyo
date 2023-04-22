17c643ec65b0631ec02496569d2c9d9434ff5406
LETomppa <vilpponent@gmail.com>
2023-04-22 11:10:57 +0300

    alkujuttu

---
+++ app/src/main/java/com/example/lutemon/Lutemon.java
@@ -0,0 +1,5 @@
+package com.example.lutemon;
+
+public class Lutemon {
+
+}
---
+++ app/src/main/java/com/example/lutemon/fragments/FragmentFight.java
@@ -0,0 +1,27 @@
+package com.example.lutemon.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.lutemon.R;
+
+
+public class FragmentFight extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fight, container, false);
+    }
+}
